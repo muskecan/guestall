af5fd56f22252bdeb2363b85f5c6048d312ec1d2
muskecan <terranova1996@gmail.com>
2017-12-05 15:21:38 +0300

    Update guestall.py

--- guestall.py
+++ guestall.py
@@ -2,9 +2,9 @@ import os
 n = raw_input("Do you want to install guest additions for VMWare or VirtualBox? \n Press 1 for VMWare, 2 for VirtualBox: ");
 os.system('sudo apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y');
 os.system('echo "Update part is done"');
-if n==1:
+if n==2:
         os.system('sudo apt-get install virtualbox-guest-x11 -y');
-elif n==2:
-        os.system('sudo apt-get install install open-vm-tools-desktop fuse -y')
+elif n==1:
+        os.system('sudo apt-get install install open-vm-tools-desktop fuse -y');
 else:
     print "You have to select 1 or 2."
